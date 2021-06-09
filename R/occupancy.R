bc9fee47c870f88b5dd4b87180c4c71b1eede321
Neal Fultz <nfultz@gmail.com>
2021-06-09 11:10:11 -0700

    Re-add package manual page

---
+++ R/occupancy.R
@@ -0,0 +1,10 @@
+#' occupancy: A package for computing with occupancy distributions.
+#'
+#' The occupancy package provides standard mass functions, distribution functions, quantile functions
+#' and random generation for various distributions.  These distributions occur in cases where balls are
+#' randomly allocated to bins.
+#'
+#'
+#' @docType package
+#' @name occupancy
+NULL
